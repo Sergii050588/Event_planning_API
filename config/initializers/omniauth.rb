34ddc0ce935bb100e75d9762fc4ecc496ec72122
Sergii Melnichenko <mozgoved88@mail.ru>
2017-01-24 12:49:19 +0200

    Implement Authentication with Facebook

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+require 'omniauth'
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'events#index'
+  match 'auth/:provider/callback', via: [:get], to: 'sessions#create'
+  match 'auth/failure',via: [:get], to: redirect('/')
+  match 'signout', via: [:get], to: 'sessions#destroy', as: 'signout'
+
   resources :events
+
 end
